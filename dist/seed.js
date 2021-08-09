69673c3b1dd6ce4163374283631dd1695462dcf8
吴wbh1328551759 <1328551759@qq.com>
2021-08-10 00:51:33 +0800

    成功创建数据

--- dist/seed.js
+++ dist/seed.js
@@ -12,14 +12,19 @@ var _typeorm = require("typeorm");
 
 var _User = require("./entity/User");
 
+var _Post = require("./entity/Post");
+
+var _Comment = require("./entity/Comment");
+
 (0, _typeorm.createConnection)().then( /*#__PURE__*/function () {
   var _ref = (0, _asyncToGenerator2["default"])( /*#__PURE__*/_regenerator["default"].mark(function _callee(connection) {
-    var manager, u1;
+    var manager, u1, p1, c1;
     return _regenerator["default"].wrap(function _callee$(_context) {
       while (1) {
         switch (_context.prev = _context.next) {
           case 0:
-            manager = connection.manager;
+            manager = connection.manager; // 创建 user1
+
             u1 = new _User.User();
             u1.username = 'frank';
             u1.passwordDigest = 'xxx';
@@ -27,11 +32,30 @@ var _User = require("./entity/User");
             return manager.save(u1);
 
           case 6:
-            console.log(u1.id);
-            _context.next = 9;
+            console.log(u1.id); // 创建 post1
+
+            p1 = new _Post.Post();
+            p1.title = 'Post 1';
+            p1.content = '我的第一篇博客';
+            p1.author = u1;
+            _context.next = 13;
+            return manager.save(p1);
+
+          case 13:
+            console.log(p1.content); //创建 comment1
+
+            c1 = new _Comment.Comment();
+            c1.user = u1;
+            c1.post = p1;
+            c1.content = 'Awesome!';
+            _context.next = 20;
+            return manager.save(c1);
+
+          case 20:
+            _context.next = 22;
             return connection.close();
 
-          case 9:
+          case 22:
           case "end":
             return _context.stop();
         }
--- src/migration/1628526828874-AddCreatedAtAndUpdatedAt.ts
+++ src/migration/1628526828874-AddCreatedAtAndUpdatedAt.ts
@@ -5,28 +5,28 @@ export class AddCreatedAtAndUpdatedAt1628526828874 implements MigrationInterface
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.addColumns('users', [
       new TableColumn({
-        name: 'createdAt', type: 'time', isNullable: false, default: 'now()'
+        name: 'createdAt', type: 'timestamp', isNullable: false, default: 'now()'
       }),
       new TableColumn({
-        name: 'updatedAt', type: 'time', isNullable: false, default: 'now()'
+        name: 'updatedAt', type: 'timestamp', isNullable: false, default: 'now()'
       }),
     ]);
 
     await queryRunner.addColumns('posts', [
       new TableColumn({
-        name: 'createdAt', type: 'time', isNullable: false, default: 'now()'
+        name: 'createdAt', type: 'timestamp', isNullable: false, default: 'now()'
       }),
       new TableColumn({
-        name: 'updatedAt', type: 'time', isNullable: false, default: 'now()'
+        name: 'updatedAt', type: 'timestamp', isNullable: false, default: 'now()'
       }),
     ]);
 
     return await queryRunner.addColumns('comments', [
       new TableColumn({
-        name: 'createdAt', type: 'time', isNullable: false, default: 'now()'
+        name: 'createdAt', type: 'timestamp', isNullable: false, default: 'now()'
       }),
       new TableColumn({
-        name: 'updatedAt', type: 'time', isNullable: false, default: 'now()'
+        name: 'updatedAt', type: 'timestamp', isNullable: false, default: 'now()'
       }),
     ]);
   }
