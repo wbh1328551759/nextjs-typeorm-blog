02cc2f8a227b018237c33c4e61644aaf10fea391
吴wbh1328551759 <1328551759@qq.com>
2021-08-20 18:05:42 +0800

    完成了 post 博客 post 的api

--- pages/api/v1/posts.tsx
+++ pages/api/v1/posts.tsx
@@ -1,12 +1,19 @@
 import { NextApiHandler, NextApiRequest, NextApiResponse } from 'next';
-import { getPosts } from 'lib/posts';
+import { Post } from '../../../src/entity/Post';
+import { getDatabaseConnection } from '../../../lib/getDatabaseConnection';
+import { withSession } from '../../../lib/withSession';
 
-const Posts: NextApiHandler = async (req: NextApiRequest, res: NextApiResponse) => {
-  const posts = await getPosts()
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'application/json');
-  res.write(JSON.stringify(posts));
-  res.end()
-};
+const Posts: NextApiHandler = withSession(async (req: NextApiRequest, res: NextApiResponse) => {
+  if(req.method === 'POST'){
+    const {title, content} = req.body
+    const post = new Post()
+    post.title = title
+    post.content = content
+    post.author = req.session.get('currentUser')
+    const connection = await getDatabaseConnection()
+    await connection.manager.save(post)
+    res.json(post)
+  }
+});
 
 export default Posts;
