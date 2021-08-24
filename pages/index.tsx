88c3ab9c0c915dd36169d4a033889b884e0cdf92
吴wbh1328551759 <1328551759@qq.com>
2021-08-24 23:33:19 +0800

    优化首页代码复用

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,39 +1,5 @@
-import styles from '../styles/Home.module.css';
-import React  from 'react';
-import { GetServerSideProps, NextPage } from 'next';
-import { getDatabaseConnection } from '../lib/getDatabaseConnection';
-import { Post } from '../src/entity/Post';
-import Link from 'next/link';
+import PostsIndex, {getServerSideProps} from './posts';
 
-interface Props {
-  posts: Post[];
-}
+export default PostsIndex
 
-const Home: NextPage<Props> = (props) => {
-  const {posts} = props;
-  console.log(posts);
-
-    return (
-    <div className={styles.container}>
-      <h1>文章列表</h1>
-      {posts.map(post =>
-        <Link key={post.id} href={`/posts/${post.id}`}>
-          <a>{post.title}</a>
-        </Link>
-      )}
-    </div>
-  );
-};
-
-export default Home;
-
-export const getServerSideProps: GetServerSideProps = async (context) => {
-  const connection = await getDatabaseConnection();
-  const posts = await connection.manager.find(Post);
-
-  return {
-    props: {
-      posts: JSON.parse(JSON.stringify(posts))
-    }
-  };
-};
+export {getServerSideProps}
--- pages/posts/index.tsx
+++ pages/posts/index.tsx
@@ -1,7 +1,7 @@
-import { GetStaticProps, NextPage } from 'next';
-import React from 'react';
-import { usePosts } from '../../hooks/usePosts';
-import { getPosts } from 'lib/posts';
+import React  from 'react';
+import { GetServerSideProps, NextPage } from 'next';
+import { getDatabaseConnection } from 'lib/getDatabaseConnection';
+import { Post } from 'src/entity/Post';
 import Link from 'next/link';
 
 interface Props {
@@ -9,22 +9,29 @@ interface Props {
 }
 
 const PostsIndex: NextPage<Props> = (props) => {
-  const {posts} = props
+  const {posts} = props;
+  console.log(posts);
+
   return (
     <div>
       <h1>文章列表</h1>
-      {posts.map(p =>
-        <div key={p.id}>
-        <Link href={`/posts/${p.id}`}><a>{p.id}</a></Link>
-      </div>)}
+      {posts.map(post =>
+        <div>
+          <Link key={post.id} href={`/posts/${post.id}`}>
+            <a>{post.title}</a>
+          </Link>
+        </div>
+      )}
     </div>
   );
 };
 
 export default PostsIndex;
 
-export const getStaticProps: GetStaticProps = async () => {
-  const posts = await getPosts();
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const connection = await getDatabaseConnection();
+  const posts = await connection.manager.find(Post);
+
   return {
     props: {
       posts: JSON.parse(JSON.stringify(posts))
