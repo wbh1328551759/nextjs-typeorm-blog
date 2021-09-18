8384432a1e7103360bee72e29cb0de2fcb023fd1
吴wbh1328551759 <1328551759@qq.com>
2021-09-18 16:50:26 +0800

    update deploy.sh

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,1 +1,9 @@
-echo Hello > /tmp/wbh
+docker start 5985e5a2f860 &&
+cd /home/blog/app &&
+git pull origin main &&
+yarn install --production=false &&
+yarn build &&
+docker kill app &&
+docker build . -t wbh-blog/node-web-app &&
+docker run --name app --network=host -p 3000:3000 -d wbh-blog/node-web-app &&
+echo 'OK!'
